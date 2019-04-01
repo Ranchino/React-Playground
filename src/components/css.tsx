2d7c0c8fd99daccc60e0e11685ed55fbec442e08
Ranj Bahadin <ranjbahadin@live.se>
2019-04-01 13:49:50 +0200

    update

---
+++ src/components/css.tsx
@@ -0,0 +1,14 @@
+//// Alternativ för att importera från CSS fil
+
+/* import {CSSProperties } from "react";
+
+export const centerdContent: CSSProperties = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: 0
+}; */
--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,4 +1,14 @@
-import * as React from "react";
+/* import * as React from "react";*/
+import React, {CSSProperties} from "react";
+
+// Alternativ för att importera från CSS fil
+/* import {centerdContent} from '../components/css'
+ */
+interface Props{
+    compiler: String;
+    framework: String;
+}
+
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +16,19 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1 style={centerdContent} > 
+        Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
+
+
+export const centerdContent: React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    margin: 0
+};
